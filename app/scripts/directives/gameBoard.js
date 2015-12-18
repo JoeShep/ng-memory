27c4d68f5c8b3161d1a21882ed89ec63be43f6fa
Joe Shepherd <joeshepmedia@gmail.com>
2015-12-18 15:37:37 -0600

    Compare two cards when flipped; prevent clicking on more than two cards;

--- app/scripts/directives/gameBoard.js
+++ app/scripts/directives/gameBoard.js
@@ -1,33 +1,51 @@
-'use strict';
 angular.module('MemoryGame')
-  .directive('gameCard', function() {
+  .directive('gameCard', function($window) {
+    'use strict';
+
+    function checkMatch(cards) {
+      console.log('There are two flipped cards', cards);
+      var card1 = cards[0].id,
+          card2 = cards[1].id;
+
+      if( card1 === card2) {
+        console.log('match!');
+        cards = [].slice.call(cards);
+        cards.forEach(function(card) {
+          setTimeout( function() {
+            angular.element(card).addClass('matched')
+          },1000 );
+        });
+      } else {
+        console.log('no match');
+      }
+    }
     return {
       restrict: 'E',
       templateUrl: 'views/partials/_game_cards.html',
       link: function(scope, elem) {
         elem.bind('click', function() {
-          console.log(elem);
-          // vanilla JS method used in attempt to do this with no jQuery: https://developer.mozilla.org/en-US/docs/Web/API/Element/querySelector
-          // var pic = elem.querySelector(".pic");
-          // console.log("Pic", pic);
-          elem.flip({
-            direction:'rl',
-            speed: 200,
-            color: 'white',
-            onEnd: function(){
-              // if($('.flipped:eq(1)').length){
-              //   // checkMatch();
-              //   counter += 1;
-              //   $('#flip-count').text(' ' + counter);
-              // }
+          var card = angular.element(elem[0].children[0].children[0]);
+          if ( angular.element(card).hasClass('matched') ) {
+            return;
+          }
+          var flippedCards = $window.document.getElementsByClassName('flipped');
+          var matchedCards = $window.document.getElementsByClassName('matched');
+
+          if (
+              !card.hasClass('flipped') && flippedCards.length < 2 ) {
+            angular.element(card).addClass('flipped');
+            console.log('after adding class', flippedCards.length);
+            if ( flippedCards.length === 2 ) {
+              checkMatch(flippedCards);
             }
-          });
-          // pic.removeClass("invisible");
+          } else {
+            angular.element(card).removeClass('flipped');
+          }
         });
-        elem.bind('mouseover', function() {
+      elem.bind('mouseover', function() {
           elem.css('cursor', 'pointer');
-        });
-      }
-    };
+      });
+    }
+   };
   }
 );
